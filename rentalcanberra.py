0c9ea3667566eadce95d6f793351323036c975b9
Arvee1 <160126772+Arvee1@users.noreply.github.com>
2024-05-13 22:10:29 +1000

    Update rentalcanberra.py

--- rentalcanberra.py
+++ rentalcanberra.py
@@ -13,6 +13,7 @@ from audiorecorder import audiorecorder
 from langchain.memory import ConversationBufferMemory
 from langchain_community.llms import Replicate
 from langchain.chains import ConversationChain
+from langchain_core.prompts.prompt import PromptTemplate
 
 # initialize
 r = sr.Recognizer()
@@ -63,10 +64,19 @@ llm = Replicate(
     model_kwargs={"temperature": 0.75, "max_length": 500, "top_p": 1},
 )
 
+template = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know.
+
+Current conversation:
+{history}
+Human: {input}
+AI Assistant:"""
+PROMPT = PromptTemplate(input_variables=["history", "input"], template=template)
+
 conversation_buf = ConversationChain(
+    prompt=PROMPT,
     llm=llm,
-    memory=ConversationBufferMemory(),
-    return_only_outputs=True
+    memory=ConversationBufferMemory(ai_prefix="AI Assistant"),
+    # memory=ConversationBufferMemory(),
 )
 
 # The UI Part
